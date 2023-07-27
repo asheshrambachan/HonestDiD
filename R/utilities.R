89e797038bfe55a476f9ebf4b020097ee703b446
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2023-07-27 14:34:28 -0400

    #36 Stop if sigma is not symm PSD

--- R/utilities.R
+++ R/utilities.R
@@ -45,3 +45,14 @@ basisVector <- function(index = 1, size = 1){
   v[index] = 1
   base::return(v)
 }
+
+.stopIfNotSymmPSD <- function (sigma) {
+  # Check that sigma is PSD
+  if ( base::isSymmetric(sigma) ) {
+    if ( base::any(base::eigen(sigma, TRUE, only.values=TRUE)$values < 0) ) {
+      base::stop("sigma must be a positive semi-definite symmetric matrix")
+    }
+  } else {
+    base::stop("sigma must be a symmetric matrix")
+  }
+}
